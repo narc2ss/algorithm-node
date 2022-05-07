4a7dca268a3a9649a2134b28cd4b14042fd250f4
narc2ss <private.narc2ss@gmail.com>
2022-05-08 03:25:49 +0900

    feat : 키패드누르기를추가한다

--- programmers/Lv.1/키패드누르기.js
+++ programmers/Lv.1/키패드누르기.js
@@ -10,43 +10,29 @@ function solution(numbers, hand) {
   const lp = [3, 0];
   const rp = [3, 2];
 
+  const update = (p, np, char) => {
+    p[0] = np[0];
+    p[1] = np[1];
+    answer += char;
+  };
+
   numbers.forEach((num) => {
     for (let i = 0; i < 4; i++) {
       for (let j = 0; j < 3; j++) {
         if (num !== keypad[i][j]) continue;
 
-        if ([1, 4, 7].includes(num)) {
-          lp[0] = i;
-          lp[1] = j;
-          answer += "L";
-        } else if ([3, 6, 9].includes(num)) {
-          rp[0] = i;
-          rp[1] = j;
-          answer += "R";
-        } else {
+        if ([1, 4, 7].includes(num)) update(lp, [i, j], "L");
+        else if ([3, 6, 9].includes(num)) update(rp, [i, j], "R");
+        else {
           const leftDistance = getDistance(lp, i, j);
           const rightDistance = getDistance(rp, i, j);
-          if (leftDistance < rightDistance) {
-            lp[0] = i;
-            lp[1] = j;
-            answer += "L";
-          } else if (leftDistance > rightDistance) {
-            rp[0] = i;
-            rp[1] = j;
-            answer += "R";
-          } else {
-            if (hand === "left") {
-              lp[0] = i;
-              lp[1] = j;
-              answer += "L";
-            } else {
-              rp[0] = i;
-              rp[1] = j;
-              answer += "R";
-            }
+          if (leftDistance < rightDistance) update(lp, [i, j], "L");
+          else if (leftDistance > rightDistance) update(rp, [i, j], "R");
+          else {
+            if (hand === "left") update(lp, [i, j], "L");
+            else update(rp, [i, j], "R");
           }
         }
-
         break;
       }
     }
